3c26d84b77f6b6d7905ccccfa9a6be20e2afc83e
Agarwal <u435750@global.ual.com>
2025-09-08 12:15:56 +0530

    First commit

---
+++ src/main/java/com/ual/NewgitdemoApplication.java
@@ -0,0 +1,13 @@
+package com.ual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewgitdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewgitdemoApplication.class, args);
+	}
+
+}
